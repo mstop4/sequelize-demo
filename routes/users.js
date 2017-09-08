65a5df87853ef2b140387bda867228be76d7bc8e
mstop4 <mstop4@gmail.com>
2017-09-08 15:17:47 -0400

    finished

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models');
+
+var User = db.User;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  User.findAll()
+    .then(function (users) {
+      res.json(users);
+    });
+});
+
+router.post('/', function(req, res, next) {
+  User.create({
+    first_name: req.query.firstName,
+    last_name: req.query.lastName,
+    bio: req.query.bio,
+    email: req.query.email
+  })
+    .then(function (user) {
+      res.json(user);
+    });
 });
 
 module.exports = router;
